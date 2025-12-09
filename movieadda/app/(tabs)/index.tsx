f2d6b91f45d8c182d4102abf7a737e7e9abd93cd
himanshu <himanshutaviyad0072@gmail.com>
2025-12-09 14:14:26 +0530

    feat: added movieadda app with tabs features

---
+++ movieadda/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 bg-red-400  justify-center  items-center"
+    >
+      <Text className="text-6xl text-primary">Welcome</Text>
+    </View>
+  );
+}
